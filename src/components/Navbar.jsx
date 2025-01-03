238af54a0ba5a932a0cf89e96c6536e2d9d446c3
Znackt <Znackt@proton.me>
2025-01-03 06:40:11 -0800

    Added The LightDark Switch Component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,16 @@
-import Logo from "../Assests/Images/logo.png";
+import LightDarkToggle from "./Light_Dark";
+import '../styles/theme.css';
 
 const Navbar = () => {
   return (
-    <nav className="bg-white shadow-lg">
-      <div className="flex max-w-7xl mx-auto px-3 py-4 justify-between">
-        <div className="flex items-center gap-1 cursor-pointer">
-          <img src={Logo} alt="Logo" className="h-10 w-auto" />
-          <h2 > <b>A</b>nalytics Module</h2>
+    <nav className="shadow-lg transition-all duration-200">
+      <div className=" flex max-w-7xl mx-auto px-3 py-4 justify-between">
+        <div className="flex items-center gap-1 cursor-pointer ">
+          <h2 className="text-2xl text-[#2C2C2C] light:text-[#000000] transition-colors duration-200"> <b>A</b>nalytics Module</h2>
         </div>
 
         <div className="flex items-center gap-1 cursor-pointer">
-          
+          <LightDarkToggle/>
         </div>
       </div>
     </nav>
