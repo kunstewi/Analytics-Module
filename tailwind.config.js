3065b2f5c0be0de9bb5c28966a40d397c6845a5e
Znackt <Znackt@proton.me>
2025-01-03 10:22:37 -0800

    New fontfamilies were added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'envyRegular': ['Envy Code Regular', 'monospace'],
+        'envyPropo': ['Envy Code Propo', 'monospace'],
+        'envyBold': ['Envy Code Bold', 'monospace'],
+      },
+    },
   },
   plugins: [],
 }
