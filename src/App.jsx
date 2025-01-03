1fc16d5713a9749839a2277eb51d7fa5f4cadded
Znackt <Znackt@proton.me>
2025-01-03 11:44:03 -0800

    Started creating the copyright footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+// import AnalyzingPage from './components/AnalyzingPage';
+import CopyrightFooter from './components/copyrightFooter';
 import LandingPage from './components/LandingPage';
 import { ThemeProvider } from './components/Light_Dark';
 import Navbar from './components/Navbar';
@@ -9,6 +11,7 @@ const App = () => {
       <div className='min-h-screen'>
         <Navbar/>
         <LandingPage/>
+        <CopyrightFooter/>
       </div>
     </ThemeProvider>
   )
