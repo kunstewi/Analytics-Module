39b4636e222c9fe2cb4681ead8beb782886aedf9
Znackt <Znackt@proton.me>
2025-01-03 02:12:13 -0800

    Created a basic nav for now

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import Logo from "../Assests/Images/logo.png";
+import LightandDark from "./LightandDark";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-white shadow-lg">
+      <div className="flex max-w-7xl mx-auto px-3 py-4 justify-between">
+        <div className="flex items-center gap-1 cursor-pointer">
+          <img src={Logo} alt="Logo" className="h-10 w-auto" />
+          <h2 > <b>A</b>nalytics Module</h2>
+        </div>
+
+        <div className="flex items-center gap-1 cursor-pointer">
+          <LightandDark/>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
