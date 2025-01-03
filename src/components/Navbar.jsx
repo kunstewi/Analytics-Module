820aec2e521d8285a1ca256402658c32515fe3fd
Znackt <Znackt@proton.me>
2025-01-03 11:57:56 -0800

    Changed the font style and weight

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <nav className="shadow-lg transition-all duration-200">
       <div className=" flex max-w-7xl mx-auto px-3 py-4 justify-between">
         <div className="flex items-center gap-1 cursor-pointer ">
-          <h2 className="text-2xl text-[#2C2C2C] light:text-[#000000] transition-colors duration-200"> <b>A</b>nalytics Module</h2>
+          <h2 className="text-2xl text-[#2C2C2C] font-envyBold font-black transition-colors duration-200"> <b>A</b>nalytics Module</h2>
         </div>
 
         <div className="flex items-center gap-1 cursor-pointer">
