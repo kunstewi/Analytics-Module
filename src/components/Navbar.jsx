798cf393438d9452518dd992801bcd7351339834
Znackt <Znackt@proton.me>
2025-01-04 23:36:28 +0530

    Removed the unnecessary tailwind properties

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,22 @@
 import LightDarkToggle from "./Light_Dark";
-import '../styles/theme.css';
+import "../styles/theme.css";
 
 const Navbar = () => {
   return (
     <nav className="shadow-lg transition-all duration-200">
       <div className=" flex max-w-7xl mx-auto px-3 py-4 justify-between">
         <div className="flex items-center gap-1 cursor-pointer ">
-          <h2 className="text-2xl text-[#2C2C2C] font-envyBold font-black transition-colors duration-200"> <b>A</b>nalytics Module</h2>
+          <h2 className="text-2xl text-[#2C2C2C] font-envyBold font-black">
+            <b>A</b>nalytics Module
+          </h2>
         </div>
 
         <div className="flex items-center gap-1 cursor-pointer">
-          <LightDarkToggle/>
+          <LightDarkToggle />
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
