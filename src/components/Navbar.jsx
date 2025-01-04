04f81f22aeedad918b7eb63f6a13aaeb0babd26b
Znackt <Znackt@proton.me>
2025-01-05 02:11:26 +0530

    Added the searchbox in the navbar which helps user to find texts from any page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,19 @@
 import LightDarkToggle from "./Light_Dark";
-import "../styles/theme.css";
+import SearchBox from "./SearchBox";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="shadow-lg transition-all duration-200">
       <div className=" flex max-w-7xl mx-auto px-3 py-4 justify-between">
-        <div className="flex items-center gap-1 cursor-pointer ">
-          <h2 className="text-2xl text-[#2C2C2C] font-envyBold font-black">
+        <Link to="/" className="flex items-center gap-1 cursor-pointer ">
+          <h2 className="text-2xl text-[#2C2C2C] hover:text-[#174b9e] font-envyBold font-black">
             <b>A</b>nalytics Module
           </h2>
-        </div>
+        </Link>
 
         <div className="flex items-center gap-1 cursor-pointer">
+          <SearchBox />
           <LightDarkToggle />
         </div>
       </div>
