1bb9f13cfaf871d416cdc689911835f6dfd9cdad
Znackt <Znackt@proton.me>
2025-01-06 02:02:00 +0530

    Added some new routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ThemeProvider } from "./components/Light_Dark";
+import AnalyzingPage from "./components/Pages/AnalyzingPage";
 import LandingPage from "./components/Pages/LandingPage";
-
+import NotFound from "./components/Pages/NotFound";
+import Signin from "./components/Pages/Signin";
 import Navbar from "./components/Navbar";
 import Docs from "./components/Docs";
 import "./styles/theme.css";
-import AnalyzingPage from "./components/Pages/AnalyzingPage";
-
 const App = () => {
   return (
     <BrowserRouter>
-      <ThemeProvider>
-        <div className="min-h-screen">
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/docs/*" element={<Docs />} />
-          </Routes>
-          <AnalyzingPage/>
-        </div>
-      </ThemeProvider>
+      <div className="min-h-screen">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/docs/*" element={<Docs />} />
+          <Route path="/analyzing" element={<AnalyzingPage />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/signin" element={<Signin />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 };
