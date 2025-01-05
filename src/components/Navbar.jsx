1c60600e83ba86a1a64ec45ed89c63c93b50143c
Znackt <Znackt@proton.me>
2025-01-06 02:04:15 +0530

    Added the github repo link and also Toggle button for theme switching

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,32 @@
-import LightDarkToggle from "./Light_Dark";
 import { Link } from "react-router-dom";
+import Toggle from "./Toggle";
+import { Github } from "lucide-react";
 
 const Navbar = () => {
   return (
-    <nav className="shadow-lg transition-all duration-200">
+    <nav className="shadow-2xl transition-all duration-200">
       <div className=" flex max-w-7xl mx-auto px-3 py-4 justify-between">
         <Link to="/" className="flex items-center gap-1 cursor-pointer ">
-          <h2 className="text-2xl text-[#2C2C2C] hover:text-[#174b9e] font-envyBold font-black">
-            <b>A</b>nalytics Module
+          <h2 className="text-2xl hover:text-[#779dda] font-envyBold font-black">
+            <b className="text-3xl font-serif">A</b>nalytics <b className="text-3xl font-serif">M</b>odule
           </h2>
         </Link>
 
-        <div className="flex items-center gap-1 cursor-pointer">
-          <LightDarkToggle />
+        <div className="flex items-center gap-2">
+          
+          <Link 
+            to="https://github.com/Znackt/Analytics-Module" 
+            target="_blank"
+            className="p-2 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-600 transition-colors duration-200 flex items-center"
+          >
+            <Github className="w-5 h-5" />
+          </Link>
+
+          <button className="p-2 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800 transition-colors duration-200">
+            <Toggle />
+          </button>
         </div>
+
       </div>
     </nav>
   );
