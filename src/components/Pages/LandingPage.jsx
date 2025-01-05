752e374310ab52a6a0bc7a162d8556cb06a2ac7f
Znackt <Znackt@proton.me>
2025-01-06 02:05:33 +0530

    Made the landing page responsive

--- src/components/Pages/LandingPage.jsx
+++ src/components/Pages/LandingPage.jsx
@@ -5,25 +5,27 @@ const LandingPage = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="min-h-5/6 justify-between pt-20 sm:pt-40">
-      <div className="flex flex-col text-2xl sm:text-4xl md:text-6xl lg:text-8xl w-full mx-auto font-black font-envyBold text-center ">
+    <div className="min-h-screen justify-between sm:justify-center md:justify-center items-center pt-20 sm:pt-40">
+      <div className="flex flex-col text-xl sm:text-3xl md:text-4xl lg:text-6xl w-auto mx-auto font-black font-envyBold text-center ">
         <h1>
-          Unlock The Power Of Your Social Media Posts
+          Unlock The Power Of Your <br /> Social Media Posts
         </h1>
       </div>
-      <div className="text-base  sm:text-lg md:text-3xl max-w-5xl mx-auto font-thin font-envyPropo text-center pt-6">
+      <div className="text-xs sm:text-base md:text-lg lg:2xl w-auto font-thin font-envyPropo text-center pt-4 px-5">
         <h2>
-          Analyze engagement across platforms. Make data-driven decisions to
+          Analyze engagement across platforms. <br /> Make data-driven decisions to
           boost your online presence.
         </h2>
       </div>
-      <div className="flex items-center justify-center mt-8 mr-10">
-        <button className="bg-[#2e2d2d] hover:bg-[#020202] text-[#f4f5f0] font-bold mr-4 py-3 px-5 rounded-md">
+      <div className="flex flex-col sm:flex-row items-center justify-center mt-4 sm:mr-10 gap-4 sm:gap-4 px-4 sm:px-0">
+        <button 
+        onClick={() => navigate("/analyzing")}
+        className="w-full sm:w-auto bg-[#2e2d2d] hover:bg-[#020202] text-[#fff] font-bold py-2 sm:py-3 px-4 sm:px-5 rounded-md text-sm sm:text-base transition-all duration-300 transform hover:scale-105">
           Start Analyzing
         </button>
         <button
           onClick={() => navigate("/docs")}
-          className="bg-[#3bab6a] hover:bg-[#19c2b1] text-[#020202] font-bold mr-2 py-3 px-5 rounded"
+          className="w-full sm:w-auto bg-[#3bab6a] hover:bg-[#19c2b1] text-[#fff] font-bold py-2 sm:py-3 px-4 sm:px-5 rounded text-sm sm:text-base transition-all duration-300 transform hover:scale-105"
         >
           Learn More
         </button>
