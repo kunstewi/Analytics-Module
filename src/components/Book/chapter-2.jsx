c842979964d0858f22ab3b7a2dcd9c87d37be03f
Znackt <Znackt@proton.me>
2025-01-07 11:55:16 +0530

    added new contents

--- src/components/Book/chapter-2.jsx
+++ src/components/Book/chapter-2.jsx
@@ -1,8 +1,21 @@
 const Chapter2 = () => {
   return (
-    <div className="chapter-content">
-      <h1>Chapter 2: Setup</h1>
-      <p>This is the content for Chapter 2.</p>
+    <div className="chapter-content pb-10 px-5">
+      <h1 className="text-4xl pb-5 font-extrabold text-blue-700 border-b-2 border-blue-300">
+        Chapter 2: Key Features of NX-Analytics
+      </h1>
+      <section className="mb-8">
+        
+        <div className="bg-blue-50 p-4 rounded-lg text-gray-700 mt-10">
+          <ul className="list-disc text-lg leading-relaxed bg-blue-50 rounded-lg pl-5">
+              <li className="mb-2">Real-time social media data processing</li>
+              <li className="mb-2">Advanced sentiment analysis using GPT technology</li>
+              <li className="mb-2">Scalable data storage with DataStax Astra DB</li>
+              <li className="mb-2">Custom workflow creation through Langflow</li>
+              <li className="mb-2">Interactive visualization dashboards</li>
+            </ul>
+        </div>
+      </section>
     </div>
   );
 };
